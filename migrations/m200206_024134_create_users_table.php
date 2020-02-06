8828756736f0182e1583765a66a7f5e987c4c46d
andrew_marhai <buk2018irinam@gmail.com>
2020-02-06 09:46:37 +0300

    Первый коммит
    Шаблон проекта собран, готов к раработке

---
+++ migrations/m200206_024134_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%users}}`.
+ */
+class m200206_024134_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%users}}', [
+            'id' => $this->primaryKey(),
+            'email' => $this->string()->notNull()->unique(),
+            'hash' => $this->string()->notNull()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%users}}');
+    }
+}
